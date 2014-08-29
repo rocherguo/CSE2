5a8c18f49018ef9d35a2627f8daf9d71c26cd292
rocherguo <shg217@lehigh.edu>
2014-08-29 14:18:02 +0000

    updated HelloWorld

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+///////////////////////////////////////////
+//Bian Chen
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the statement with box around it
+    System.out.println("    -----------     ");
+    System.out.println("    | WELCOME |     ");
+    System.out.println("    -----------     ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \/ \/ \/ \/ \/ \ ");
+    System.out.println("|-S--H--G--2--1--7-|");
+    System.out.println(" \ /\ /\ /\ /\ /\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("One of my dogs ate my homework. No kidding.");
+    
+    }
+}
